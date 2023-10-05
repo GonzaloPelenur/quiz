1b017d19480294daf94ca4f26282be35afc8723c
GonzaloPelenur <gonpelenur@gmail.com>
2023-10-05 16:29:49 -0400

    main to quiz navigation

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
+import Link from "next/link";
 
-export default function Page (){
+export default function Page() {
   // console.log(process.env.AIRTABLE_BASE_ID);
 
   return (
     <body>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  </body>
-  )
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Link href="/quiz">QUIZ</Link>
+    </body>
+  );
 }
