cfbb87e71ba85d1208b6c80e6ce5e1cf1dc389df
GonzaloPelenur <gonpelenur@gmail.com>
2023-10-06 16:16:30 -0400

    got gpt working

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -7,13 +7,50 @@ import Card from "../../components/Card";
 const Page = () => {
   const [processedData, setProcessedData] = useState([]);
   const [selectedOptions, setSelectedOptions] = useState([]);
+  const [generatedText, setGeneratedText] = useState([`""`]);
   const handleCardSelect = (questionId, option) => {
     setSelectedOptions((prevOptions) => [
       ...prevOptions.filter((item) => item.questionId !== questionId),
       { questionId, option },
     ]);
-    console.log(`Selected option for question ${questionId}: ${option}`);
+    // console.log(`Selected option for question ${questionId}: ${option}`);
   };
+
+  const handleSubmit = async () => {
+    // const prompt = selectedOptions.map((opt) => opt.option).join(" ");
+    const optionsList = selectedOptions.map((item) => item.option);
+    if (optionsList.length !== processedData.length) {
+      console.error("Empty or invalid prompt");
+      return;
+    }
+    const selectedPrompt = JSON.stringify(optionsList);
+    const availableOptions = JSON.stringify(
+      processedData.map((item) => [item.One, item.Two])
+    );
+    // console.log(availableOptions);
+    // console.log(selectedPrompt);
+    // console.log("Prompt:", prompt);
+    try {
+      const response = await fetch("api/openAI", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ selectedPrompt, availableOptions }), // This sends the prompt to the server
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        // console.log("Recieved data:", data);
+        setGeneratedText(data.result);
+      } else {
+        console.error("API request failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   useEffect(() => {
     async function fetchData() {
       try {
@@ -35,6 +72,7 @@ const Page = () => {
 
   return (
     <div>
+      <h1>Use this AI embeded Quiz to chose a Lab!</h1>
       {processedData.map((options, index) => (
         <div key={index}>
           <Card
@@ -47,6 +85,12 @@ const Page = () => {
           />
         </div>
       ))}
+      <div style={{ textAlign: "center" }}>
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <p>{generatedText}</p>
+      </div>
     </div>
   );
 };
