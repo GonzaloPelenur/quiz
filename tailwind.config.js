f50914d92acad2abd34600e4c12495a0a70011cb
GonzaloPelenur <gonpelenur@gmail.com>
2023-10-19 14:46:27 -0400

    fixed tailwindcss error on build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
-// tailwind.config.js
-const { nextui } = require("@nextui-org/react");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // ...
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 };
